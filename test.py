45995920591f017ee51ae9ec25cccf512c19ccd1
MujtabaAhmad21 <mma212005@gmail.com>
2024-07-17 18:30:19 +0500

    Updated test.py

--- test.py
+++ test.py
@@ -562,83 +562,376 @@
 #     response = input("I don't know that flavor! Try again: ")
 # print(f"Ok, {response} ice cream coming right up!")
 
-nums = [3,4,6,7,9,0,2,3,4,5,6,7,8,1,2,3]
-print(nums.count(2))
+# nums = [3,4,6,7,9,0,2,3,4,5,6,7,8,1,2,3]
+# print(nums.count(2))
 
-nums = [1,2,3,4,5]
-nums.reverse()
-print(f"Numbers reversed: {nums}")
+# nums = [1,2,3,4,5]
+# nums.reverse()
+# print(f"Numbers reversed: {nums}")
 
-nums = [2,8,1,9,3]
-nums.sort()
-print(f"Numbers sorted in ascending order: {nums}")
-nums.sort(reverse=True)
-print(f"Numers sorted in descending order: {nums}")
+# nums = [2,8,1,9,3]
+# nums.sort()
+# print(f"Numbers sorted in ascending order: {nums}")
+# nums.sort(reverse=True)
+# print(f"Numers sorted in descending order: {nums}")
 
-colors = ["red", "orange", "purple", "green"]
-colors.sort()   # uppercase and lowercase alphabet matters in sorting
-print(f"Colors sorted from a-z: {colors}") 
+# colors = ["red", "orange", "purple", "green"]
+# colors.sort()   # uppercase and lowercase alphabet matters in sorting
+# print(f"Colors sorted from a-z: {colors}") 
 
-print([1,2,3] == [1,2,3])
-print([1,2,3] is [1,2,3])
+# print([1,2,3] == [1,2,3])
+# print([1,2,3] is [1,2,3])
 
-evens = [2,4,6]
-evens2 = evens
-print(evens is evens2)
-print(id(evens))
-print(id(evens2))
+# evens = [2,4,6]
+# evens2 = evens
+# print(evens is evens2)
+# print(id(evens))
+# print(id(evens2))
 
-birthday = "03/27/2020"
-birthdaylist = birthday.split("/")
-print(birthdaylist)
+# birthday = "03/27/2020"
+# birthdaylist = birthday.split("/")
+# print(birthdaylist)
 
-fullname = "Teddy Richard Smith"
-fullnamelist = fullname.split(" ")
-print(fullnamelist)
+# fullname = "Teddy Richard Smith"
+# fullnamelist = fullname.split(" ")
+# print(fullnamelist)
 
-fruits = ["Apple", "Kiwi", "Pear"]
-fruitslist = " ".join(fruits)
-print(fruitslist)
+# fruits = ["Apple", "Kiwi", "Pear"]
+# fruitslist = " ".join(fruits)
+# print(fruitslist)
 
-newfruitslist = "!!!".join(fruits)
-print(newfruitslist)
+# newfruitslist = "!!!".join(fruits)
+# print(newfruitslist)
 
-letters = ['M', 'A', 'S', 'H']
-newletters = "".join(letters)
-print(newletters)
+# letters = ['M', 'A', 'S', 'H']
+# newletters = "".join(letters)
+# print(newletters)
 
-stringlist = "!".join("hello")
-print(stringlist)
+# stringlist = "!".join("hello")
+# print(stringlist)
 
-#      LIST UNPACKING:- 
-RGBlist = [255,43,19]
-red, green, blue = RGBlist
-print("Red is ", red)
-print("Blue is: ", blue)
-print("Green is:", green)
+# #      LIST UNPACKING:- 
+# RGBlist = [255,43,19]
+# red, green, blue = RGBlist
+# print("Red is ", red)
+# print("Blue is: ", blue)
+# print("Green is:", green)
 
-race_results = ['bill', 'ted', 'tammy', 'jimbo', 'billybob', 'pierre']
-gold, silver, bronze, *losers = race_results
-print("Gold winner: ", gold)
-print("Silver winner: ", silver) 
-print("Bronze winner: ", bronze)
-print("Losers are: ", *losers)
+# race_results = ['bill', 'ted', 'tammy', 'jimbo', 'billybob', 'pierre']
+# gold, silver, bronze, *losers = race_results
+# print("Gold winner: ", gold)
+# print("Silver winner: ", silver) 
+# print("Bronze winner: ", bronze)
+# print("Losers are: ", *losers)
 
-list1 = [12,9,3,7]
-list2 = list1.copy()  # this method returns a shallow copy of this list
+# list1 = [12,9,3,7]
+# list2 = list1.copy()  # this method returns a shallow copy of this list
 # if it had some nested data structures, those would not have been copied.
 
 # another way to copy is using a slice
 
-list2 = list1[:]
-print(list2)
+# list2 = list1[:]
+# print(list2)
 
 # if wanted to deepcopy means the nested data structure also to be copied
 # then :-
-import copy
-list1 = [2,9,['a', 'b'], 7]
-list2 = copy.deepcopy(list1)
+# import copy
+# list1 = [2,9,['a', 'b'], 7]
+# list2 = copy.deepcopy(list1)
 
-import pyfiglet
-ascii_banner = pyfiglet.figlet_format("Hello!!")
-print(ascii_banner)
+# import pyfiglet
+# ascii_banner = pyfiglet.figlet_format("Hello!!")
+# print(ascii_banner)
+
+
+#  DICTIONARIES
+#      |
+#      |
+#      |
+#     \|/
+#      -
+
+# cannot use a list in a dictionary because it is mutable and dictionaries
+# only hold immutable types.
+
+# to access a value :- dict[key]
+# movie = {
+#     "title":"Amadeus",
+#     "imdb_score": 8.3
+# }
+# print(movie["title"])
+
+# movie["title"] = "Blade Runner"
+# print(movie["title"])
+
+# movie["imdb_score"] += 0.5
+# print(movie["imdb_score"])
+
+# movie["rating"] = "R"
+# print(movie)
+
+# prices = {
+#     "arugala": 1.10,
+#     "basil": 2.54,
+#     "blackberries": 4.93,
+#     "blueberries": 2.88,
+#     "coconut": 7.15,
+#     "fennel": 3.36
+# }
+
+# product = input("What product are you buying? ")
+# if product in prices:
+#     price = prices[product]
+#     print(f"{product} is ${price}")
+# else:
+#     print("I don't have that today, sorry!")
+
+# another way to do the same thing
+
+# product = input("What product are you buying? ")
+# price = prices.get(product)
+# if price:
+#     print(f"{product} is ${price}")
+# else:
+#     print("I don't have that today, sorry!")
+
+# prices.pop("coconut")
+# print(prices)
+
+# dict_1 = {"a": 1, "b": 2, "c": 3}
+# pop_item = dict_1.popitem()
+# print(pop_item)
+
+# test_scores = {
+#     "Marsha": 78,
+#     "Baker": 69,
+#     "Rosa": 92,
+#     "Leif": 97,
+#     "Peng": 61,
+#     "Juan": 73,
+#     "Esteban": 84,
+#     "Amir": 91,
+#     "Sakura": 99
+# }
+
+# for student in test_scores.keys():
+#     print(student)
+
+# total = 0
+# for score in test_scores.values():
+#     total += score
+# print("The average is = ", total/len(test_scores))
+
+# for items in test_scores.items():
+#     print(items)
+
+# # You can also unpack the items like this :-
+
+# for k,v in test_scores.items():
+#     print(k, v)
+
+# max_score = 0
+# best_student = ""
+# for student, score in test_scores.items():
+#     if score > max_score:
+#         max_score = score
+#         best_student = student
+# print(f"Highest score was {max_score} by {best_student}")
+
+# d1 = {'a': 1, 'b': 2}
+# d2 = {'c': 3, 'd': 4, 'e': 5, 'a':'apple'}
+# d1.update(d2)
+# print(d1)
+
+# dict1 = {"a": 1, "b": 2}
+# dict2 = {"c": 3, "d": 4}
+# dict3 = {**dict1, **dict2}
+# print(dict3)
+
+# another way to perform the same thing and it is a newer version
+
+# dict3 = dict1 | dict2       # this is called a dict union
+# print(dict3)
+
+# produce = {
+#     "aragula": {
+#         "price": 1.10,
+#         "qty": 61,
+#         "organic": True,
+#         "producer": "Blue Kitty Farms"
+#     },
+#     "coconut": {
+#         "price": 7.15,
+#         "qty": 12,
+#         "organic": False,
+#         "producer": "Tropical Dream Farm"
+#     }
+# }
+
+# print(produce["coconut"]["price"])
+
+# test_scores = {
+#     "Marsha": [78, 80, 89],
+#     "Baker": [69, 65, 52]
+# }
+
+# waitlist = [
+#     {
+#         "email": "tiff@gmail.com",
+#         "location": "USA",
+#         "first_name": "Tiffany"
+#     },
+#     {
+#         "email": "bob123@yahoo.com",
+#         "location": "california",
+#         "first_name": "Bobby"
+#     }
+# ]
+
+#  DICTIONARY PEAK EXERCISE :-
+
+# peak = {
+#     "name": "Castle Peak",
+#     "height": "14264",
+#     "summit_log": [],
+#     "call_reception": {
+#         "AT&T": "no reception",
+#         "T-Mobile": "poor"
+#     }
+# }
+
+# h1 = {"height": 14265}
+
+# peak["range"] = "Elk Mountains"
+# peak["first_climbed"] = 1873
+# peak.update(h1)
+
+# peak["call_reception"]["verizon"] = "good"
+
+# peak["summit_log"].append("Mujtaba")
+
+# heightvar = peak.pop("height")
+# peak["elevation"] = heightvar
+
+# for values in peak.values():
+#     print(values)
+
+# for k,v in peak.items():
+#     print(f"{k} -> {v}")
+
+# peak.clear()
+
+# print(peak)
+
+#  TUPLES & SETS
+#      |
+#      |
+#      |
+#     \|/
+#      -
+
+# tuples are immutable
+
+# single_tuple = ("First") --> wrong
+# single_tuple = ("first",)  #--> right
+# single_tuple = "First",    #--> right
+
+# evens = {2,4,6,8,2,4}    # only unique values are stored in sets which 
+# print(evens)      # are immutable, can't add a list in it and are unoredered
+# evens.add(10)
+# evens.add(7)      # they also don't have indexes
+# evens.remove(7)
+
+# webdev = {'SQL', 'CSS', 'HTML', 'JS', 'PYTHON'}
+# datasci = {'R', 'SQL', 'PYTHON'}
+
+# print(webdev & datasci)
+# print(webdev | datasci)
+# print(webdev - datasci)
+# print(datasci - webdev)
+
+#  RETURNING TO FUNCTIONS
+#      |
+#      |
+#      |
+#     \|/
+#      -
+
+# def average(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total/len(args)
+# print("The average is = ", average(1,2,3,4,5,6,7,8,8))
+
+# def count_stuff(*args):  # collects the remaining arguments in a tuple
+#     print(f"You passed me {len(args)} arguments")
+# print(count_stuff(1,2,6,8,9,5,30,1,34,6))
+
+# def sum(*nums):
+#     total = 0
+#     for num in nums:
+#         total += num
+#     return total
+# print("The sum is = ", sum(9,9,9,9,99,9,9,9,9,9,9,9,9))
+
+# def print_ages(**kwargs):
+#     for k,v in kwargs.items():
+#         print(f"{k} is {v} years old")
+# print_ages(Mujtaba=19 , Taha=18 , Zohaib=15)
+
+# when defining functions, the order of the parameters matters:
+# paramters - *args - default parameters - **kwargs
+
+# def display_info(person, *args, status = "single", **kwargs):
+#     print(f"person is: {person}")
+#     print(f"status is: {status}")
+#     print(f"args is: {args}")
+#     print(f"kwargs is: {kwargs}")
+
+# display_info("colt", "purple", 4,5,6,7,8,9)
+# display_info("colt", "purple", 24,5,6,7, status="married")
+# display_info("colt", "purple", 24,5,6,7, status="married", age=87, mood="thrilled")
+
+# def add_thrice(val, lst = None):
+#     if lst is None:
+#         lst = []
+#     lst.append(val)
+#     lst.append(val)
+#     lst.append(val)
+#     return lst
+# print(add_thrice(7, [1,2,3]))
+# print(add_thrice(99))
+# print(add_thrice(0))
+
+# you can also unpack when calling a function to pass a list in *args
+
+# ARGSKWARGS problem set
+
+def contains_pickle(*args):
+    if "pickle" in args:
+        return True
+    else:
+        return False
+    
+print(contains_pickle(1,2, 'blue', 23))
+
+def count_fails(*args):
+    count = 0
+    for num in args:
+        if num <= 50:
+            count += 1
+    return count
+
+print(count_fails(50, 41, 47, 74, 76, 81))
+
+def get_top_students(**kwargs):
+    top_students = []
+    for student, score in kwargs.items():
+        if score >= 90:
+            top_students.append(student)
+    return top_students
+
+print(get_top_students(tim=91, stacy=83, carlos=97, jim=69))
+print(get_top_students(colt=61, elton=76))
+print(get_top_students(kitty=80, blue=95, toad=91, ))
