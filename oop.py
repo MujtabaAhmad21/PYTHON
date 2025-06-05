cb338f2daa60fa8288c8ce844bcee8ad87502b84
Mujtaba Ahmad <mma212005@gmail.com>
2025-06-05 15:17:29 +0500

    Introduction to OOP

---
+++ oop.py
@@ -0,0 +1,29 @@
+class car:
+    def __init__(self, brand, color):
+        self.brand = brand
+        self.color = color
+
+    def honk(self):
+        print("BEEP BEEP!")
+
+my_car = car('Porsche', 'Black')
+
+print(f"Car name is: {my_car.brand}")
+print(f"Car color is: {my_car.color}")
+print(f"Car is in traffic: car does ")
+my_car.honk()
+
+def greet():
+    print("Welcome")
+
+prices = [55,68,77,36]
+
+x = 5
+city = "London"
+is_open = True
+
+print(type(greet))
+print(type(prices))
+print(type(x))
+print(type(city))
+print(type(is_open))
