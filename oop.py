68f00c2c78988ac569372be693d44b7659a117f9
Mujtaba Ahmad <mma212005@gmail.com>
2025-06-05 16:29:21 +0500

    Added Inheritance concepts

--- oop.py
+++ oop.py
@@ -27,3 +27,37 @@ print(type(prices))
 print(type(x))
 print(type(city))
 print(type(is_open))
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        print("Making a sound")
+
+class Dog(Animal):
+    def __init__(self,name, breed, age):
+        # initialize attribute
+        super().__init__(name)
+        self.breed = breed
+        self.age = age
+
+    def sound(self):
+        print("WOOF")
+
+class Cat(Animal):
+    def __init__(self, name, breed, age):
+        super().__init__(name)
+        self.breed = breed
+        self.age = age
+
+    def sound(self):
+        print("MEOW")
+
+my_dog = Dog("Jax", "Bulldog", 5)
+my_cat = Cat("Lily", "Ragdoll", 2)
+
+animals = [my_cat, my_dog]
+
+for animal in animals:
+    animal.sound()
